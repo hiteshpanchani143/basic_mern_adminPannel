f4fb438b650606761dc73baf795191ce3caa29eb
hiteshpanchani143 <hiteshpanchaniweb@gmail.com>
2024-08-17 00:15:18 +0530

    Secure User Password using Bcrypt.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,5 @@
 const User = require("../models/userModel");
-
+// const bcrypt = require("bcryptjs");
 const home = async (req, res) => {
   try {
     res.status(200).send({ msg: "welcome to home page" });
@@ -11,12 +11,24 @@ const home = async (req, res) => {
 const register = async (req, res) => {
   try {
     const { username, email, password, phone } = req.body;
+    console.log(req.body);
     const userExits = await User.findOne({ email });
     if (userExits) {
       return res.status(400).json({ msg: "email already exits" });
     }
-    const user = await User.create({ username, email, password, phone });
-    res.status(200).send({ msg: "welcome to register page", user });
+
+    // const saltRound = await bcrypt.genSalt(10)
+    // const hashPassword =await bcrypt.hash(password,10); // you can directly write 10 salt
+
+    // Note : hashPassword process in userModel.
+    const user = await User.create({
+      username,
+      email,
+      // password: hashPassword,
+      password,
+      phone,
+    });
+    res.status(201).send({ msg: "welcome to register page", user });
   } catch (error) {
     res.status(400).send({ msg: "error in register", error: error.message });
   }
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const bcrypt = require("bcryptjs");
 const userModel = new mongoose.Schema({
   username: {
     type: String,
@@ -21,6 +21,23 @@ const userModel = new mongoose.Schema({
     type: Boolean,
     default: false,
   },
+});
+
+// hashPassword process before user create
+userModel.pre("save", async function (bext) {
+  const user = this;
+  // if not modify password
+  if (!user.isModified("password")) {
+    next();
+  }
+  try {
+    // if first time user create
+    const saltRound = await bcrypt.genSalt(10); //
+    const hashPassword = await bcrypt.hash(this.password, saltRound);
+    user.password = hashPassword;
+  } catch (error) {
+    next();
+  }
 });
 
 const User = new mongoose.model("User", userModel);

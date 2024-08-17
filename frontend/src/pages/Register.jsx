df4c8110ab9c40b5f2457f925c75afeaee70de15
hiteshpanchani143 <hiteshpanchaniweb@gmail.com>
2024-08-18 01:12:26 +0530

    complete Register flow with handle form without call api in frontend

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,10 +1,102 @@
+import { useState } from "react";
+
 const Register = () => {
-    return (
-      <div>
-        <h1>Register Page</h1>
-      </div>
-    );
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    password: "",
+    phone: "",
+  });
+
+  const handleInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setUser((prev) => ({ ...prev, [name]: value }));
   };
-  
-  export default Register;
-  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+  };
+  return (
+    <section>
+      <main>
+        <div className="section-registration">
+          <div className="container grid grid-two-cols">
+            <div className="registration-image">
+              <img
+                src="/images/form.png"
+                alt="register image"
+                width="500"
+                height="500"
+              />
+            </div>
+            <div className="registration-form">
+              <h1 className="main-heading mb-3">registration form</h1>
+              <br />
+              <form onSubmit={handleSubmit}>
+                <div>
+                  <label htmlFor="username">username</label>
+                  <input
+                    type="text"
+                    name="username"
+                    placeholder="enter your username"
+                    id="username"
+                    required
+                    autoComplete="off"
+                    value={user.username}
+                    onChange={handleInput}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="email">email</label>
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="enter your email"
+                    id="email"
+                    required
+                    autoComplete="off"
+                    value={user.email}
+                    onChange={handleInput}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="password">password</label>
+                  <input
+                    type="password"
+                    name="password"
+                    placeholder="enter your password"
+                    id="password"
+                    required
+                    autoComplete="off"
+                    value={user.password}
+                    onChange={handleInput}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="phone">phone</label>
+                  <input
+                    type="number"
+                    name="phone"
+                    placeholder="enter your phone"
+                    id="phone"
+                    required
+                    autoComplete="off"
+                    value={user.phone}
+                    onChange={handleInput}
+                  />
+                </div>
+                <br />
+                <button type="submit" className="btn btn-submit">
+                  register now
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </main>
+    </section>
+  );
+};
+
+export default Register;

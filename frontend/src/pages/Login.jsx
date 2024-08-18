6ba04ad6d83a09aa43c92ccd05321fea22be511a
hiteshpanchani143 <hiteshpanchaniweb@gmail.com>
2024-08-18 13:33:46 +0530

    Compete Contact Form  With Storing Data in State

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -31,38 +31,40 @@ const Login = () => {
             <div className="registration-form">
               <h1 className="main-heading mb-3">login form</h1>
               <br />
-              <form onSubmit={handleSubmit}>
-                <div>
-                  <label htmlFor="email">email</label>
-                  <input
-                    type="email"
-                    name="email"
-                    placeholder="enter your email"
-                    id="email"
-                    required
-                    autoComplete="off"
-                    value={user.email}
-                    onChange={handleInput}
-                  />
-                </div>
-                <div>
-                  <label htmlFor="password">password</label>
-                  <input
-                    type="password"
-                    name="password"
-                    placeholder="enter your password"
-                    id="password"
-                    required
-                    autoComplete="off"
-                    value={user.password}
-                    onChange={handleInput}
-                  />
-                </div>
-                <br />
-                <button type="submit" className="btn btn-submit">
-                  login now
-                </button>
-              </form>
+              <section className="section-form">
+                <form onSubmit={handleSubmit}>
+                  <div>
+                    <label htmlFor="email">email</label>
+                    <input
+                      type="email"
+                      name="email"
+                      placeholder="enter your email"
+                      id="email"
+                      required
+                      autoComplete="off"
+                      value={user.email}
+                      onChange={handleInput}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="password">password</label>
+                    <input
+                      type="password"
+                      name="password"
+                      placeholder="enter your password"
+                      id="password"
+                      required
+                      autoComplete="off"
+                      value={user.password}
+                      onChange={handleInput}
+                    />
+                  </div>
+                  <br />
+                  <button type="submit" className="btn btn-submit">
+                    login now
+                  </button>
+                </form>
+              </section>
             </div>
           </div>
         </div>
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -33,64 +33,66 @@ const Register = () => {
             <div className="registration-form">
               <h1 className="main-heading mb-3">registration form</h1>
               <br />
-              <form onSubmit={handleSubmit}>
-                <div>
-                  <label htmlFor="username">username</label>
-                  <input
-                    type="text"
-                    name="username"
-                    placeholder="enter your username"
-                    id="username"
-                    required
-                    autoComplete="off"
-                    value={user.username}
-                    onChange={handleInput}
-                  />
-                </div>
-                <div>
-                  <label htmlFor="email">email</label>
-                  <input
-                    type="email"
-                    name="email"
-                    placeholder="enter your email"
-                    id="email"
-                    required
-                    autoComplete="off"
-                    value={user.email}
-                    onChange={handleInput}
-                  />
-                </div>
-                <div>
-                  <label htmlFor="password">password</label>
-                  <input
-                    type="password"
-                    name="password"
-                    placeholder="enter your password"
-                    id="password"
-                    required
-                    autoComplete="off"
-                    value={user.password}
-                    onChange={handleInput}
-                  />
-                </div>
-                <div>
-                  <label htmlFor="phone">phone</label>
-                  <input
-                    type="number"
-                    name="phone"
-                    placeholder="enter your phone"
-                    id="phone"
-                    required
-                    autoComplete="off"
-                    value={user.phone}
-                    onChange={handleInput}
-                  />
-                </div>
-                <br />
-                <button type="submit" className="btn btn-submit">
-                  register now
-                </button>
-              </form>
+              <section className="section-form">
+                <form onSubmit={handleSubmit}>
+                  <div>
+                    <label htmlFor="username">username</label>
+                    <input
+                      type="text"
+                      name="username"
+                      placeholder="enter your username"
+                      id="username"
+                      required
+                      autoComplete="off"
+                      value={user.username}
+                      onChange={handleInput}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="email">email</label>
+                    <input
+                      type="email"
+                      name="email"
+                      placeholder="enter your email"
+                      id="email"
+                      required
+                      autoComplete="off"
+                      value={user.email}
+                      onChange={handleInput}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="password">password</label>
+                    <input
+                      type="password"
+                      name="password"
+                      placeholder="enter your password"
+                      id="password"
+                      required
+                      autoComplete="off"
+                      value={user.password}
+                      onChange={handleInput}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="phone">phone</label>
+                    <input
+                      type="number"
+                      name="phone"
+                      placeholder="enter your phone"
+                      id="phone"
+                      required
+                      autoComplete="off"
+                      value={user.phone}
+                      onChange={handleInput}
+                    />
+                  </div>
+                  <br />
+                  <button type="submit" className="btn btn-submit">
+                    register now
+                  </button>
+                </form>
+              </section>
             </div>
           </div>
         </div>

3f06f775e3cc371cca5b880ba3ee0a578c55bfec
hiteshpanchani143 <hiteshpanchaniweb@gmail.com>
2024-08-18 13:08:44 +0530

    Complete footer component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Service from "./pages/Service";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 function App() {
   return (
     <BrowserRouter>
@@ -19,6 +20,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/service" element={<Service />} />
       </Routes>
+      <Footer/>
     </BrowserRouter>
   );
 }
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <section className="footer_section">
+      <div className="footer">
+        <p>&copy;hiteshPanchani {new Date().getFullYear()}</p>
+      </div>
+    </section>
+  );
+};
+
+export default Footer;

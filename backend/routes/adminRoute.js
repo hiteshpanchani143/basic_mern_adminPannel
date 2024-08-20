91a701493185f51ffcb5ac1d24349841845f4d0d
hiteshpanchani143 <hiteshpanchaniweb@gmail.com>
2024-08-20 19:06:47 +0530

    Backend -Admin Authentication: Verifying Whether User is an Admin or Not

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -2,9 +2,25 @@ const express = require("express");
 const adminController = require("../controllers/adminControllers");
 const router = express.Router();
 const authMiddleware = require("../middelware/auth/authMiddleware");
+const adminMiddleware = require("../middelware/admin/adminMiddleware");
 
-router.get("/user", authMiddleware, adminController.getAllUser);
-router.get("/service", authMiddleware, adminController.getAllServices);
-router.get("/contact", authMiddleware, adminController.getAllContact);
+router.get(
+  "/user",
+  authMiddleware,
+  adminMiddleware,
+  adminController.getAllUser
+);
+router.get(
+  "/service",
+  authMiddleware,
+  adminMiddleware,
+  adminController.getAllServices
+);
+router.get(
+  "/contact",
+  authMiddleware,
+  adminMiddleware,
+  adminController.getAllContact
+);
 
 module.exports = router;
